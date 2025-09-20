83af1aaf476219951a769c5bb18d712133fc0aa4
jadenryu <jadenryu@gmail.com>
2025-09-20 12:04:54 -0400

    serper

--- llm_stuff/serper.py
+++ llm_stuff/serper.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import asyncio
+from crawl4ai import *
+
 url = "https://google.serper.dev/search?q=apple+inc&apiKey=b2a719347945479e449681749ad9e8d759a9b225"
 
 payload = {}
@@ -17,3 +20,16 @@ for result in data.get('organic', []):
     print("Snippet:", result.get('snippet'))
     print("Link:", result.get('link'))
     print("---")
+browser_config = BrowserConfig(
+    browser_mode="builtin",  # This is the key setting!
+    headless=False           # Can be headless or not
+)
+async def main():
+    async with AsyncWebCrawler(config = browser_config) as crawler:
+        result = await crawler.arun(
+            url="https://www.nbcnews.com/business",
+        )
+        print(result.markdown)
+
+if __name__ == "__main__":
+    asyncio.run(main())
